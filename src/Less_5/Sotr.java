584cee2c32a632f8d616d92ed96ee37a76270943
sollino <zurg@inbox.ru>
2019-05-19 19:27:06 +0300

    дз

--- src/Less_5/Sotr.java
+++ src/Less_5/Sotr.java
@@ -1,4 +1,47 @@
 package Less_5;
 
 public class Sotr {
-}
+
+//    ФИО, должность, email, телефон, зарплата, возраст
+
+    private String FIO;
+    private String dolg;
+    private String email;
+    private String phone;
+    private int ZP;
+    private int age;
+
+
+    public Sotr(String FIO,
+                String dolg,
+                String email,
+                String phone,
+                int ZP,
+                int age) {
+        System.out.println(FIO + " " + dolg + " " + email + " " + phone + " " + ZP + " " + age);
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFIO() {
+        return FIO;
+    }
+
+    public  void search (){
+        if (age > 40){
+            System.out.println(FIO);
+        }
+    }
+
+
+      }
+
