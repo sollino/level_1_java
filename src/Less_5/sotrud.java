e8688a69769b108f0a862c46ae31473f5c7071db
sollino <zurg@inbox.ru>
2019-05-19 19:26:59 +0300

    дз

--- src/Less_5/sotrud.java
+++ src/Less_5/sotrud.java
@@ -1,4 +1,40 @@
 package Less_5;
 
 public class sotrud {
-}
+        public static  void main (String[]args){
+
+
+
+            Sotr[ ] idS = new Sotr[5];
+            idS [0] = new Sotr("Пупкин Сиван", "инженер ", "frog@pop.com",
+                    "8763834", 25000, 24  );
+            idS [1] = new Sotr("Зупкин Диван", "принтер ", "frog@pop.com",
+                    "6785434", 28876, 32  );
+            idS [2] = new Sotr("Рупкин Риван", "осенизатор", "frog@pop.com",
+                    "1244677", 50000, 46  );
+            idS [3] = new Sotr("Цупкин Живан", "краб", "frog@pop.com",
+                    "3465764", 20000, 19  );
+            idS [4] = new Sotr("Лупкин Киван", "индейка добытчица", "frog@pop.com",
+                    "5686454", 70000, 60  );
+
+
+           String  oneAge; {
+                for (int i = 0; i < idS.length; i++) {
+                    if (idS[i].getAge()> 40);
+                    System.out.println(idS[i].getFIO());
+
+                }
+
+            }
+
+
+
+            }
+
+
+
+        }
+
+
+
+
